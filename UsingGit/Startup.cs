6c7d8605d348950c2dc894a0855025d154170a88
Satinder <Satinder@TR-SATINDER>
2017-02-10 18:17:41 +0530

    Add project files.

---
+++ UsingGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsingGit.Startup))]
+namespace UsingGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
